b33ff9da170ea482c08033dec6a282a6ea1c47bd
mati <mati@s09-pc05>
2013-10-30 10:37:23 +0100

    Clase AlmacenBean creada

---
+++ src/main/java/net/daw/bean/AlmacenBean.java
@@ -0,0 +1,67 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.daw.bean;
+
+/**
+ *
+ * @author mati
+ */
+public class AlmacenBean {
+    
+    private int id;
+    private String nombre;
+    private String localidad;
+    
+    public AlmacenBean(){
+    
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the localidad
+     */
+    public String getLocalidad() {
+        return localidad;
+    }
+
+    /**
+     * @param localidad the localidad to set
+     */
+    public void setLocalidad(String localidad) {
+        this.localidad = localidad;
+    }
+    
+    
+    
+    
+    
+}
